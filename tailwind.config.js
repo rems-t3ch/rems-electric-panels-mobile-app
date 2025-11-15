2905d2ae4401570063046c12a059672de26f2660
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-11-14 22:10:04 -0500

    chore(tailwind): extend theme with accent, aqua and coral colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
       colors: {
         primary: '#1A1A1A',
         secondary: '#77E7D9',
+        accent: '#F27465',
+        aqua: '#00FFFF',
+        coral: '#FF7F50',
       },
     },
   },
