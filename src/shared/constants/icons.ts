830a5658fbe6f8d89df1fe8329e11828eb8042ee
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-11-14 23:58:51 -0500

    chore: add edit and trash icons and export icons constant.

---
+++ src/shared/constants/icons.ts
@@ -0,0 +1,10 @@
+import TrashIcon from "@assets/trash-icon.png";
+import EditIcon from "@assets/edit-icon.png";
+
+/**
+ * Icons used throughout the application.
+ */
+export const icons = {
+    trash: TrashIcon,
+    edit: EditIcon,
+};
